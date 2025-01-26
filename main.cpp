865537b660ca6e5cfc8cca8777b5f8e294eacc98
Grafscorp <golyandringraf@gmail.com>
2025-01-26 19:39:05 +0300

    set stylesheet

--- main.cpp
+++ main.cpp
@@ -1,10 +1,15 @@
 #include "mainwindow.h"
-
+#include <QFile>
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    QFile styleFile(":/Wstartpage.qss");
+    styleFile.open(QFile::ReadOnly);
+    QString style(styleFile.readAll());
+    a.setStyleSheet(style);
     MainWindow w;
     w.show();
     return a.exec();
