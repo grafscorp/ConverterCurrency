ba121af2c133c3ceee72b6748fdf3184d769728c
Grafscorp <golyandringraf@gmail.com>
2025-01-26 18:58:43 +0300

    Update mainwindow.h

--- mainwindow.h
+++ mainwindow.h
@@ -3,7 +3,9 @@
 
 #include <QMainWindow>
 #include <convertercurrencyapi.h>
-
+#include <currencyfield.h>
+#include <QKeyEvent>
+#include <QPushButton>
 QT_BEGIN_NAMESPACE
 namespace Ui {
 class MainWindow;
@@ -18,10 +20,27 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+protected:
+    void keyPressEvent(QKeyEvent *event) override
+    {
+        if(event->key() == Qt::Key_Enter)
+        {
+            getDataCurrency();
+            qInfo()<<"OPK";
+        }
+    }
+
 private:
     Ui::MainWindow *ui;
     ConverterCurrencyApi* converterApi;
+    CurrencyField *currencyInputField;
+    CurrencyField *currencyOutputField;
+    QPushButton *convertButton;
     void getDataCurrency();
+public slots:
+    void onReady(QMap<QString,QVariant>);
+    void onError(QString);
+    void updateData();
 
 };
 #endif // MAINWINDOW_H
