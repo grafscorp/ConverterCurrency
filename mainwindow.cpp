6b85132af5cbaab4db0e35203ba8c424d1f44f74
Grafscorp <golyandringraf@gmail.com>
2025-01-26 18:59:58 +0300

    Setting ui

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -12,6 +12,32 @@ MainWindow::MainWindow(QWidget *parent)
     currencyOutputField = new CurrencyField(this);
     convertButton = new QPushButton("Convert",this);
 
+    currencyOutputField->setOnlyReadMode(true);
+
+    currencyInputField->setValidatorForInputLineEdit(new QDoubleValidator(0,1000000,2,this));
+    currencyInputField->setCurrencyLine("1");
+    convertButton->setSizePolicy(QSizePolicy::Fixed,QSizePolicy::Fixed);
+
+
+    auto listCurrencyData = converterApi->getCurrencyData();
+
+    for(auto currencyData:listCurrencyData)
+    {
+        currencyInputField->addElementComboBoxCurrency(currencyData.symb);
+        currencyOutputField->addElementComboBoxCurrency(currencyData.symb);
+    }
+    currencyOutputField->setCurrentIndex(1);
+
+    ui->VLayout->setAlignment(Qt::AlignCenter);
+    ui->horizontalLayout->addWidget(currencyInputField);
+    ui->horizontalLayout->addWidget(currencyOutputField);
+    ui->buttonLayout->addWidget(convertButton);
+
+    currencyInputField->getCurrencySymb();
+
+    connect(converterApi,&ConverterCurrencyApi::onReady,this,&MainWindow::onReady);
+    connect(converterApi,&ConverterCurrencyApi::onError,this,&MainWindow::onError);
+    connect(convertButton,&QPushButton::released, this,&MainWindow::updateData);
 }
 
 MainWindow::~MainWindow()
